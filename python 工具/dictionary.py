7b07fff44665a6b06dd70379ea21dc562fb57309
LDlornd <lornd@qq.com>
2019-09-19 19:22:53 +0800

    文本转二维码功能实现，同时统一三个源程序的码风

--- python 工具/dictionary.py
+++ python 工具/dictionary.py
@@ -1,38 +1,63 @@
 import json
 import re
 import os
+import sys
 
 dictionary = {}
 reverseDictionary = {}
-tip = "输入格式为 \"key\" + 空格 + : + 空格 + \"value\" + ,，如 \" \"a\" : \"b\", \""
+tip = "非大括号行的输入格式为 \"key\" + 空格 + : + 空格 + \"value\" + ,，如 \" \"a\" : \"b\", \""
 
-print("欢迎使用 python 字典反转系统，请输入您的原始字典：")
-print("注意：" + tip)
+def dictionaryReverse():
+    dictionary.clear()
+    reverseDictionary.clear()
+    print("请输入您的原始字典：")
+    print("建议您第一行输入左大括号，最后一行输入右大括号")
+    print("注意：" + tip)
+    while True:
+        try:
+            s = input()
+        except:
+            break
+        if s == "{": continue
+        elif s == "}": break
+        else:
+            if re.match("\".+\"\\s:\\s\".+\",", s) == None:
+                print("请注意您的输入格式" + tip)
+            else: 
+                key, val = s.split(" : ")
+                key = key[1 : len(key) - 1]
+                val = val[1 : len(val) - 2]
+                dictionary[key] = val
+    for i in dictionary:
+        if reverseDictionary.get(dictionary[i]):
+            reverseDictionary[dictionary[i]].append(i)
+        else:
+            reverseDictionary[dictionary[i]] = [i]
+    for i in reverseDictionary:
+        if len(reverseDictionary[i]) == 1:
+            reverseDictionary[i] = reverseDictionary[i][0]
+    print(json.dumps(reverseDictionary, indent = 4))
+
+def inputERROR():
+    print("请确保您的输入为 0 或 1")
+
 while True:
+    os.system("cls")
+    print("欢迎使用 python 字典反转系统，请选择您要进行的操作：")
+    print("1: 反转字典")
+    print("0: 退出")
     try:
-        s = input()
-    except EOFError:
-        break
-    if s == '{': continue
-    elif s == '}': break
+        option = input()
+    except:
+        inputERROR()
     else:
-        if re.match("\".+\"\\s:\\s\".+\",", s) == None:
-            print("请注意您的输入格式" + tip)
-        else: 
-            key, val = s.split(" : ")
-            key = key[1 : len(key) - 1]
-            val = val[1 : len(val) - 2]
-            dictionary[key] = val
-for i in dictionary:
-    if reverseDictionary.get(dictionary[i]):
-        reverseDictionary[dictionary[i]].append(i)
-    else:
-        reverseDictionary[dictionary[i]] = [i]
-for i in reverseDictionary:
-    if len(reverseDictionary[i]) == 1:
-        reverseDictionary[i] = reverseDictionary[i][0]
-print(json.dumps(reverseDictionary, indent = 4))
-os.system("pause")
+        if option == "0":
+            break
+        elif option != "1":
+            inputERROR()
+        else:
+            dictionaryReverse()
+    os.system("pause")
 
 '''
 {
---
+++ python 工具/txtToQrcode.py
@@ -0,0 +1,29 @@
+import qrcode
+import os
+
+def txtToQrcode():
+    try:
+        f = open(s + ".txt")
+    except:
+        print("您输入的文件不存在，请检查文件路径或文件名是否正确")
+    else:
+        jpg = qrcode.make(f.read())
+        jpg.save(s + ".jpg")
+        print("二维码生成成功！")
+        f.close()
+
+while True:
+    os.system("cls")
+    print("欢迎使用 python 二维码生成系统，本系统可将 txt 文件转换为二维码，请选择您要进行的操作：")
+    print("欢迎使用 python 二维码生成系统，本系统可将 txt 文件转换为二维码，请输入文件名（不带后缀），或者输入 0 以退出系统：")
+    print("注意：若使您想直接输入文件名，而不带上路径的话，您需要将文件放在程序运行目录下")
+    try:
+        s = input()
+    except:
+        print("请输入正确的文件名或者 0 !")
+    else:
+        if s == "0":
+            break
+        else:
+            txtToQrcode()
+    os.system("pause")
